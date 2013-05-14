70f30d3390c9ee842820a3f288e94b3e08bb4f7a
Don Park <donp@donp.org>
2013-05-14 16:02:03 -0700

    first communication with bitcoind

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+var fs = require('fs')
+
 var ApiServer = require('apiserver')
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('transactions.db');
 
+var db = require('./lib/db')
+var btcd = require('./lib/bitcoind')
+var config = JSON.parse(fs.readFileSync('config.json'))
+
+// connect to the database
+db.setup(sqlite3)
+// connect to bitcoind
+btcd.setup(config.bitcoind.uri)
+
+// startup synchronization
+db.sync(btcd, function(err, count){
+	if(err) {
+		console.log('err  '+err)
+	} else {
+		console.log('block count '+JSON.stringify(count.body))
+	}
+})
