5031758e9a18c4c12e346a5f28acbd24ef6ee348
Don Park <donp@donp.org>
2013-05-15 09:37:44 -0700

    simplify db module

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ var btcd = require('./lib/bitcoind')
 var config = JSON.parse(fs.readFileSync('config.json'))
 
 // connect to the database
-db.setup(sqlite3)
+db.setup(sqlite3, 'db/transactions.db')
 // connect to bitcoind
 btcd.setup(config.bitcoind.uri)
 
 // startup synchronization
-db.sync(btcd, function(count){
-  console.log('block count '+count)
+db.load_block_height(function(block_height){
+  btcd.unprocessed_transactions_since(block_height, function(transactions){
+    transactions.each(function(transaction){
+      db.save_transaction(transaction)
+    })
+  })
 })
--- test/db.js
+++ test/db.js
@@ -1,16 +1,46 @@
-var vows = require('vows'),
+var fs = require('fs'),
+    vows = require('vows'),
     assert = require('assert'),
     nodemock = require('nodemock'),
     sqlite3 = require('sqlite3');
 
 var db = require('../lib/db.js')
 
+function db_file_setup(){
+  var test_db_filename = 'db/test.db'
+  if(fs.existsSync(test_db_filename)){
+    fs.unlinkSync(test_db_filename)
+  }
+  db.setup(sqlite3, test_db_filename)
+}
+
+db_file_setup()
+
 vows.describe('db').addBatch({
   'sqlite3': {
     topic: db,
     'setup': function(db){
-      db.setup(sqlite3)
       assert(db.connected)
     }
+  },
+  'missing load_block_height': {
+    topic: function(){
+      db.load_block_height(this.callback)
+    },
+    'no height': function(height){
+      assert.isUndefined(height)
+    }
+  },
+  'existing load_block_height': {
+    topic: function(){
+      db.save_block_height(1, function(){
+        console.log("saveback!")
+        db.load_block_height(this.callback)
+      })
+    },
+    'existing height': function(height){
+      assert.equal(height, 1)
+    },
   }
+
 }).export(module)
