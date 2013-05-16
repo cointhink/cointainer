be16b60defca559bd45787f92fb3db7fe534b3de
Don Park <donp@donp.org>
2013-05-16 11:55:17 -0700

    refactor into cointainer.js

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
+// nodejs libs
 var fs = require('fs'),
     timers = require('timers')
 
+// npm libs
 var ApiServer = require('apiserver')
 var sqlite3 = require('sqlite3').verbose();
 
-var db = require('./lib/db')
-var btcd = require('./lib/bitcoind')
+// local libs
+var db = require('./lib/db'),
+    btcd = require('./lib/bitcoind'),
+    cointainer = require('./lib/cointainer')
+
+// config file
 var config = JSON.parse(fs.readFileSync('config.json'))
 
-// set bitcoid location
+// setup bitcoind
 btcd.setup(config.bitcoind.uri)
 
-// connect to the database
+// setup the database
 db.setup(sqlite3, 'db/transactions.db')
+
+// setup the manager
+cointainer.setup(db, btcd)
 
 console.log("Begin sync every "+config.sync_rate+" sec.")
 db.load_block_hash(function(block_hash){
   if(block_hash){
-    block_report(block_hash)
+    cointainer.block_report(block_hash)
   }
 })
 
-// startup synchronization
-timers.setInterval(sync, config.sync_rate*1000)
+// start synchronization
+timers.setInterval(cointainer.sync, config.sync_rate*1000)
 
-function sync(){
-  db.load_block_hash(function(block_hash){
-    btcd.unprocessed_transactions_since(block_hash, function(bitcoin){
-      db.transaction(function(){
-        bitcoin.transactions.forEach(function(tx){
-          db.add_bitcoin_tx(tx)
-        })
-        lastblock = bitcoin.lastblock
-        db.save_block_hash(lastblock)
-        block_report(lastblock)
-      })
-    })
-  })
-}
-
-function block_report(block_hash){
-   btcd.block(block_hash, function(block){
-    console.log("Synced up to block #"+block.height+" "+(new Date(block.time*1000)))
-  })
-}
