c57327e8be7797df03274b60766b54f0e1f55908
Don Park <donp@donp.org>
2013-05-16 13:50:29 -0700

    switch to jayson json-rpc 2.0 server

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var fs     = require('fs'),
     timers = require('timers')
 
 // npm libs
-var ApiServer = require('apiserver'),
-    sqlite3   = require('sqlite3').verbose();
+var jayson  = require('jayson'),
+    sqlite3 = require('sqlite3').verbose();
 
 // local libs
 var db         = require('./lib/db'),
     btcd       = require('./lib/bitcoind'),
-    cointainer = require('./lib/cointainer')
+    cointainer = require('./lib/cointainer'),
+    rpc        = require('./lib/rpc')
 
 // config file
 var config = JSON.parse(fs.readFileSync('config.json'))
@@ -32,5 +33,4 @@ cointainer.sync()
 timers.setInterval(cointainer.sync, config.sync_rate*1000)
 
 // API
-api = new ApiServer(config.api)
-api.listen()
+jayson.server(rpc).http().listen(config.api.port)
